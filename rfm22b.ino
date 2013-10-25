d796301d93ac80c7b04b9868279986b14cbd40ff
Alexander Anashkin <baychi@mail.ru>
2013-10-25 01:27:55 -0700

    Update rfm22b.ino

--- rfm22b.ino
+++ rfm22b.ino
@@ -1,380 +1,255 @@
 // **********************************************************
-// **      RFM22B/Si4432 control functions for OpenLRS     **
-// **       This Source code licensed under GPL            **
+// Baychi soft 2013
+// **      RFM22B/23BP/Si4432 Reciever with Expert protocol **
+// **      This Source code licensed under GPL            **
 // **********************************************************
-// Latest Code Update : 2011-09-26
-// Supported Hardware : OpenLRS Tx/Rx boards (store.flytron.com)
-// Project Forum      : http://forum.flytron.com/viewforum.php?f=7
-// Google Code Page   : http://code.google.com/p/openlrs/
+// Latest Code Update : 2013-10-22
+// Supported Hardware : Expert Tiny/2G RX, Orange/OpenLRS Rx boards (store.flytron.com)
+// Project page       : https://github.com/baychi/OpenTinyRX
 // **********************************************************
- 
-#define NOP() __asm__ __volatile__("nop") 
 
- 
-// Режимы RFM для 7-го регистра
-#define RF22B_PWRSTATE_READY    01 
-#define RF22B_PWRSTATE_TX        0x09 
-#define RF22B_PWRSTATE_RX       05 
+// Функции меню терминала
+//
 
-// Режимы прерывания для 5-го регистра
-#define RF22B_Rx_packet_received_interrupt   0x02 
-#define RF22B_PACKET_SENT_INTERRUPT  04 
-#define RF22B_PWRSTATE_POWERDOWN  00 
+void printlnPGM(char *adr, char ln)   // печать строки из памяти программы
+{
+  byte b;
+  while(1) {
+    b=pgm_read_byte(adr++);
+    if(!b) break;
+    Serial.write(b);
+  }
 
- 
-unsigned char ItStatus1, ItStatus2; 
-
-unsigned char read_8bit_data(void); 
-// void to_tx_mode(void); 
-void to_ready_mode(void); 
-void send_8bit_data(unsigned char i); 
-void send_read_address(unsigned char i); 
-void _spi_write(unsigned char address, unsigned char data); 
-void RF22B_init_parameter(void); 
-
-void port_init(void);   
-unsigned char _spi_read(unsigned char address); 
-void Write0( void ); 
-void Write1( void ); 
-void timer2_init(void); 
-void Write8bitcommand(unsigned char command); 
-void to_sleep_mode(void); 
- 
- 
-//***************************************************************************** 
-//***************************************************************************** 
-
-//-------------------------------------------------------------- 
-void Write0( void ) 
-{ 
-    SCK_off;  
-    NOP(); 
-     
-    SDI_off; 
-    NOP(); 
-     
-    SCK_on;  
-    NOP(); 
-} 
-//-------------------------------------------------------------- 
-void Write1( void ) 
-{ 
-    SCK_off;
-    NOP(); 
-     
-    SDI_on;
-    NOP(); 
-     
-    SCK_on; 
-    NOP(); 
-} 
-//-------------------------------------------------------------- 
-void Write8bitcommand(unsigned char command)    // keep sel to low 
-{ 
- unsigned char n=8; 
-    nSEL_on;
-    SCK_off;
-    nSEL_off; 
-    while(n--) 
-    { 
-         if(command&0x80) 
-          Write1(); 
-         else 
-          Write0();    
-              command = command << 1; 
-    } 
-    SCK_off;
-}  
-
-
-//-------------------------------------------------------------- 
-unsigned char _spi_read(unsigned char address) 
-{ 
- unsigned char result; 
- send_read_address(address); 
- result = read_8bit_data();  
- nSEL_on; 
- return(result); 
-}  
-
-//-------------------------------------------------------------- 
-void _spi_write(unsigned char address, unsigned char data) 
-{ 
- address |= 0x80; 
- Write8bitcommand(address); 
- send_8bit_data(data);  
- nSEL_on;
-}  
-
-
-//-------Defaults 7400 baud---------------------------------------------- 
-void RF22B_init_parameter(void) 
-{ 
-  ItStatus1 = _spi_read(0x03); // read status, clear interrupt   
-  ItStatus2 = _spi_read(0x04); 
-  
-  _spi_write(0x06, 0x00);    // no interrupt: no wakeup up, lbd, 
-  _spi_write(0x07, RF22B_PWRSTATE_READY);      // disable lbd, wakeup timer, use internal 32768,xton = 1; in ready mode 
-  if(Regs4[2]!=0)  _spi_write(0x09, Regs4[2]);     // точная подстройка частоты 
-  else _spi_write(0x09, 199);     // если сброшена, используем умолчание   
-  _spi_write(0x0a, 0x05);    // выход CLK 2 МГц ?
-  _spi_write(0x0b, 0x12);    // gpio0 TX State
-  _spi_write(0x0c, 0x15);    // gpio1 RX State 
-
-#if(RX_BOARD_TYPE == 2)
-  _spi_write(0x0d, 0x0a);    // GPIO2, для управления SAW фильтром
-#endif
-#if(RX_BOARD_TYPE == 1)
-  _spi_write(0x0d, 0x1b);    // GPIO2 как индикатор принимаемыемх пакетов
-#endif
-
-  _spi_write(0x0e, 0x00);    // gpio 0, 1,2 NO OTHER FUNCTION. 
-  
-// From Expert
-  _spi_write(0x1F, 0x03);    //  Clock recovery
-  _spi_write(0x1E, 0x0A);    //  AFC timing
-  _spi_write(0x12, 0x60);    //  Режим измерения температуры -40..+85 C
-  _spi_write(0x13, 0xF8);    //  Смещение температуры ?
-  _spi_write(0x0F, 0x80);    //  АЦП для измерения температуры
-  _spi_write(0x1D, 0x40);    //  AFC enable
-
-//--------------------------
-   
-  _spi_write(0x1c, 0x26);    // IF filter bandwidth
-  _spi_write(0x20, 0x44);    // clock recovery oversampling rate  
-  _spi_write(0x21, 0x00);    // 0x21 , rxosr[10--8] = 0; stalltr = (default), ccoff[19:16] = 0; 
-  _spi_write(0x22, 0xF2);    // 0x22   ncoff =5033 = 0x13a9 
-  _spi_write(0x23, 0x7C);    // 0x23 
-  _spi_write(0x24, 0x06);    // 0x24 
-  _spi_write(0x25, 0x7D);    // 0x25 clock recovery ...
-  _spi_write(0x2a, 0x1e);    // AFC limiter
-
-  _spi_write(0x30, 0xAC);    // Mode: enable packet handler, msb first, enable crc CCITT, 
-  _spi_write(0x32, 0x8C);    // Header: 0x32address enable for headere byte 0, 1,2,3, receive header check for byte 0, 1,2,3 
-  _spi_write(0x33, 0x0A);    // header 3, 2, 1,0 used for head length, fixed packet length, synchronize word length 3, 2, 
-  _spi_write(0x34, 0x04);    // 2 bytes preamble 
-  _spi_write(0x35, 0x22);    // expect full preamble 
-  _spi_write(0x36, 0x2d);    // synchronize word 3
-  _spi_write(0x37, 0xd4);    // 2
-  _spi_write(0x38, 0x00);    // 1
-  _spi_write(0x39, 0x00);    // 0
-
-  _spi_write(0x3e, RF_PACK_SIZE);    // total tx 16 byte 
-   
-  _spi_write(0x6d, 0x0f);    // 7 set power max TX power 
-// 7400 bps data rate
-  _spi_write(0x6e, 0x3C); //  RATE_7400 
-  _spi_write(0x6f, 0x9F); //   
-
-  _spi_write(0x69, 0x60);    //  AGC enable
-  _spi_write(0x70, 0x2E);    //  manchester enable!!!
-  _spi_write(0x79, 0x00);    // no hopping (1-st channel)
-  _spi_write(0x7a, HOPPING_STEP_SIZE);    // 60khz step size (10khz x value) // no hopping 
-
-  _spi_write(0x71, 0x23);  // Gfsk,  fd[8] =0, no invert for Tx/Rx data, fifo mode, txclk -->gpio 
-  _spi_write(0x72, 0x0E);  // frequency deviation setting to 8750
- 
-
-  //band 434.075
- _spi_write(0x75, 0x53);   // 433075 кГц  
- _spi_write(0x76, 0x4C);    
- _spi_write(0x77, 0xE0); 
+  if(ln) Serial.println();  
 }
 
+static unsigned char regs[] = {1, 2, 3, 4, 11,12,13,14,15,16,17,18,19,20,24,25,26,28,40,41,42 } ;
 
-//-------------------------------------------------------------- 
-void send_read_address(unsigned char i) 
-{ 
- i &= 0x7f; 
-  
- Write8bitcommand(i); 
-}  
-//-------------------------------------------------------------- 
-void send_8bit_data(unsigned char i) 
-{ 
-  unsigned char n = 8; 
-  SCK_off;
-    while(n--) 
-    { 
-         if(i&0x80) 
-          Write1(); 
-         else 
-          Write0();    
-         i = i << 1; 
-    } 
-   SCK_off;
-}  
-//-------------------------------------------------------------- 
+// Держим текст в программной памяти
+//
+static char help1[] PROGMEM =  "Bind N";
+static char help2[] PROGMEM =  "Freq Corr";
+static char help3[] PROGMEM =  "Servo 150% strech num (1-12)";
+static char help4[] PROGMEM =  "Statistics enable";
+static char help5[] PROGMEM =  "Hope F1";
+static char help6[] PROGMEM =  "Hope F2";
+static char help7[] PROGMEM =  "Hope F3";
+static char help8[] PROGMEM =  "Hope F4";
+static char help9[] PROGMEM =  "Hope F5";
+static char help10[] PROGMEM =  "Hope F6";
+static char help11[] PROGMEM =  "Hope F7";
+static char help12[] PROGMEM =  "Hope F8";
+static char help13[] PROGMEM =  "Beacon F (FF=disable)";
+static char help14[] PROGMEM =  "Beacon Pmax (mWt): 0-1.2; 1-2; 2-3; 3-6; 4-12; 5-25; 6-50; 7-100";  
+static char help15[] PROGMEM =  "Beacon start time (sec)";
+static char help16[] PROGMEM =  "SAW Fmin";
+static char help17[] PROGMEM =  "SAW Fmax";
+static char help18[] PROGMEM =  "PPM mode 1st PWM chnl (1-8) [4]"; 
+static char help19[] PROGMEM =  "RSSI type: sound(0)/level(1-99=average)";
+static char help20[] PROGMEM =  "RSSI mode: level(0)/SN ratio(1)";
+static char help21[] PROGMEM =  "RSSI over PWM(chan:1-12) 0-not use";
+static char *menuAdr[] = {      // массив адресов строк 
+   help1, help2, help3, help4, help5, help6, help7, help8, help9, help10, 
+   help11, help12, help13, help14, help15, help16, help17, help18, help19, help20, 
+   help21
+};  
 
-unsigned char read_8bit_data(void) 
-{ 
-  unsigned char Result, i; 
-  
- SCK_off;
- Result=0; 
-    for(i=0;i<8;i++) 
-    {                    //read fifo data byte 
-       Result=Result<<1; 
-       SCK_on;
-       NOP(); 
-       if(SDO_1) 
-       { 
-         Result|=1; 
-       } 
-       SCK_off;
-       NOP(); 
-     } 
-    return(Result); 
-}  
-//-------------------------------------------------------------- 
-
-//----------------------------------------------------------------------- 
-void rx_reset(void) 
-{ 
-  _spi_write(0x07, RF22B_PWRSTATE_READY); 
-  _spi_write(0x7e, 36);    // threshold for rx almost full, interrupt when 1 byte received 
-  _spi_write(0x08, 0x03);    //clear fifo disable multi packet 
-  _spi_write(0x08, 0x00);    // clear fifo, disable multi packet 
-  _spi_write(0x07,RF22B_PWRSTATE_RX );  // to rx mode 
-  _spi_write(0x05, RF22B_Rx_packet_received_interrupt); 
-  ItStatus1 = _spi_read(0x03);  //read the Interrupt Status1 register 
-  ItStatus2 = _spi_read(0x04);  
-}  
-//-----------------------------------------------------------------------    
-
-void to_rx_mode(void) 
-{  
- to_ready_mode(); 
- delay(50); 
- rx_reset(); 
- NOP(); 
-}  
-
-
-//-------------------------------------------------------------- 
-void to_ready_mode(void) 
-{ 
-  ItStatus1 = _spi_read(0x03);   
-  ItStatus2 = _spi_read(0x04); 
-  _spi_write(0x07, RF22B_PWRSTATE_READY); 
-}  
-//-------------------------------------------------------------- 
-void to_sleep_mode(void) 
-{ 
-  _spi_write(0x07, RF22B_PWRSTATE_READY);  
-   
-  ItStatus1 = _spi_read(0x03);  //read the Interrupt Status1 register 
-  ItStatus2 = _spi_read(0x04);    
-  _spi_write(0x07, RF22B_PWRSTATE_POWERDOWN); 
-
-} 
-//--------------------------------------------------------------   
-  
-  
-void frequency_configurator(long frequency)
+void showRegs(void)         // показать значения регистров
 {
-/******  
-  // frequency formulation from Si4432 chip's datasheet
-  // original formulation is working with mHz values and floating numbers, I replaced them with kHz values.
+  unsigned char i,j=0,k;
   
-  unsigned int frequency_constant = (frequency/10000) - 24; // result is 19 for 430-439.990 Mhz
-
-  frequency = frequency / 10;
-  frequency = frequency - 24000;
-  
-  frequency =  frequency - (frequency_constant*1000);  
-  
-  frequency = frequency * 64; // this is the Nominal Carrier Frequency (fc) value for register setting
-  
-   
-  byte byte0 = (byte) frequency;
-  byte byte1 = (byte) (frequency >> 8);
-  
-  _spi_write(0x75, 0x40 + frequency_constant);
-  _spi_write(0x76, byte1);    
-  _spi_write(0x77, byte0); 
-**************************/
-  // no variantrs
- _spi_write(0x75, 0x53);   // 433075 кГц  
- _spi_write(0x76, 0x4C);    
- _spi_write(0x77, 0xE0); 
-
-}
-
-// Маяк из проекта KHA
-
-void beacon_tone(int16_t hz, int16_t len, byte pow) //duration is now in half seconds.
-{
-  int16_t d = 500000 / hz; // better resolution
-
-  if (d < 5) d = 5;
-
-  _spi_write(0x6d, (pow&7)|0x8);   // устанавливаем мощность
-
-#if(RX_BOARD_TYPE == 1)
-  _spi_write(0x0e, 0x04);     // зажигаем индикатор
-#else
-  Green_LED_ON
-#endif  
-
-  delay(10);
-
-#if(RX_BOARD_TYPE == 1)
-  _spi_write(0x0e, 0x00);     // гасим индикатор
-#else
-  Green_LED_OFF
-#endif  
-
-  wdt_reset();               //  поддержка сторожевого таймера
-
-  int16_t cycles = (len * 250000 / d);
-
-  for (int16_t i = 0; i < cycles; i++) {
-    SDI_on;
-    delayMicroseconds(d);
-    SDI_off;
-    delayMicroseconds(d);
+  for(int i=1; i<=REGS_NUM; i++) {
+    if(regs[j] == i) {
+      Serial.print(i);
+      Serial.write('=');
+      Serial.print(read_eeprom_uchar(i));
+      Serial.write('\t');
+      printlnPGM(menuAdr[j]);   // читаем строки из программной памяти
+      j++;
+    }
   }
 }
 
-void beacon_send(void)
+
+byte checkMenu(void)   // проверка на вход в меню
 {
-  ItStatus1 = _spi_read(0x03);   // read status, clear interrupt
-  ItStatus2 = _spi_read(0x04);
-  _spi_write(0x06, 0x00);    // no wakeup up, lbd,
-  _spi_write(0x07, RF22B_PWRSTATE_READY);      // disable lbd, wakeup timer, use internal 32768,xton = 1; in ready mode
-  _spi_write(0x09, Regs4[2]);  // подстройка частоты
-  _spi_write(0x0a, 0x05);
-  _spi_write(0x0b, 0x12);    // gpio0 TX State
-  _spi_write(0x0c, 0x15);    // gpio1 RX State
-  _spi_write(0x0d, 0x0a);    // gpio2 - управление SAW фильтром/или лампочкой
-  _spi_write(0x0e, 0x00);    // gpio2=0
-
-  _spi_write(0x70, 0x2C);    // disable manchester
-
-  _spi_write(0x30, 0x00);    //disable packet handling
-
-  _spi_write(0x79, BeaconReg[0]);    // start channel
-
-  _spi_write(0x7a, 0x06);   // 50khz step size (10khz x value) // no hopping
-
-  _spi_write(0x71, 0x12);   // trclk=[00] no clock, dtmod=[01] direct using SPI, fd8=0 eninv=0 modtyp=[10] FSK
-  _spi_write(0x72, 0x02);   // fd (frequency deviation) 2*625Hz == 1.25kHz
-
-  _spi_write(0x73, 0x00);
-  _spi_write(0x74, 0x00);    // no offset
-
-   SAW_FILT_OFF              // SAW disable
-   frequency_configurator(433);  // фиктивный вызов
-
-  _spi_write(0x6d, (BeaconReg[1]&7)|0x8);   // 5 set mid power 25mW
-  delay(10);
-  _spi_write(0x07, RF22B_PWRSTATE_TX);    // to tx mode
-  beacon_tone(440, 1 ,BeaconReg[1]);
-  beacon_tone(349, 1, BeaconReg[2]);
-  beacon_tone(175, 1, BeaconReg[3]);
-  beacon_tone(261, 1, BeaconReg[4]);
-
-  _spi_write(0x07, RF22B_PWRSTATE_READY);
+   int in; 
+   
+   if (Serial.available() > 0) {
+      in= Serial.read();             // все, что пришло, отображаем
+      if(in == 'm' || in == 'M') return 1; // есть вход в меню
+   } 
+   return 0;                        // не дождались 
 }
+
+
+void getStr(char str[])             // получение строки, завершающейся Enter от пользователя
+{
+  int in,sn=0;
+  str[0]=0;
+  while(1) {
+    if (Serial.available() > 0) {
+      if(Serial.peek() == SAT_PACK_HEADER) {  // если обнаружили заголовок от саттелита
+        str[0]='q'; str[1]=0;      // иммитируем Quit
+        menuFlag=0;              // запрещаем меню
+        return;
+      }
+       in=Serial.read();             // все, что пришло, отображаем
+       if(in > 0) {
+          Serial.write(in);
+          if(in == 0xd || in == 0xa) {
+            Serial.println();
+            return;                     // нажали Enter
+          }
+          if(in == 8) {                 // backspace, удаляем последний символ
+            if(sn) sn--;
+            continue;
+          } 
+          str[sn]=in; str[sn+1]=0;
+          if(sn < 10) sn++;             // не более 10 символов
+        }
+     } else delay(1);
+      wdt_reset();               //  поддержка сторожевого таймера
+   }
+}
+
+#define R_AVR 199               // усреднение RSSI
+
+byte margin(byte v)
+{
+   if(v < 10) return 0; 
+   else if(v>71) return 61;
+
+   return  v-10;
+}
+
+char ntxt1[] PROGMEM = "FHn: Min Avr Max";
+
+void showNoise(char str[])             // отображаем уровень шумов по каналам
+{
+  byte fBeg=0, fMax=254;
+  byte rMin, rMax;
+  word rAvr;
+  byte i,j,k;
+
+  rAvr=atoi(str+1);           // считаем параметры, если есть в виде Nbeg-end
+  if(rAvr > 0 && rAvr < 255) {
+     fBeg=rAvr;
+     for(i=2; i<10; i++) {
+      if(str[i] == 0) break;
+      if(str[i] == '-') {
+        rAvr=atoi(str+i+1);
+        if(rAvr > fBeg && rAvr < 255) fMax=rAvr;
+        break;
+      }
+    }
+  }
+  
+  RF22B_init_parameter();      // подготовим RFMку 
+  to_rx_mode(); 
+  SAW_FILT_OFF                 
  
+  printlnPGM(ntxt1);
+  
+  for(i=fBeg; i<=fMax; i++) {    // цикл по каналам
+     _spi_write(0x79, i);       // ставим канал
+     delayMicroseconds(749);
+     rMin=255; rMax=0; rAvr=0;
+     for(j=0; j<R_AVR; j++) {   // по каждому каналу 
+       delayMicroseconds(99);
+       k=_spi_read(0x26);         // Read the RSSI value
+       rAvr+=k;
+       if(k<rMin) rMin=k;         // min/max calc
+       if(k>rMax) rMax=k;
+     }
+     if(i < 10) Serial.print("  ");
+     else if(i <100) Serial.write(' ');
+     Serial.print(i);
+     k=':';
+     for(j=0; j<HOPE_NUM; j++) {   // отметим свои частоты
+        if(hop_list[j] == i) {
+          k='#';
+        }
+     }
+     Serial.write(k); Serial.write(' ');
+     print3(rMin);   
+     k=rAvr/R_AVR;  print3(k);
+     print3(rMax);
+
+     if(str[0] == 'N') {         // если надо, печатаем псевдографику 
+       rMin=margin(rMin); 
+       rMax=margin(rMax); 
+       k=margin(k); 
+
+       for(j=0; j<=rMax; j++) {                         // нарисуем псевдографик
+         if(j == k) Serial.write('*');
+         else if(j == rMin) Serial.write('<');
+         else if(j == rMax) Serial.write('>');
+         else if(j>rMin && j <rMax) Serial.write('.');
+         else Serial.write(' ');
+       }
+     }
+     
+     Serial.println();
+     wdt_reset();               //  поддержка сторожевого таймера
+  }
+}
+
+// Перенесем текст меню в память программ
+char mtxt1[] PROGMEM = "To Enter MENU Press ENTER";
+char mtxt2[] PROGMEM = "Type Reg and press ENTER, type Value and press ENTER (q=Quit; ss/sl/sa=Stat)";
+char mtxt3[] PROGMEM = "Rg=Val \tComments -----------------------";
+
+void doMenu()                       // работаем с меню
+{
+  char str[12];
+  int reg,val;
+
+  printlnPGM(mtxt1);
+  getStr(str);
+  if(str[0] == 'q' || str[0] == 'Q') return;     // Q - то quit
+  
+  while(1) {
+    printlnPGM(mtxt3);
+    showRegs();
+    printlnPGM(mtxt2);
+
+rep:  
+    getStr(str);
+    if(str[0] == 'n' || str[0] == 'N') {  // отсканировать и отобразить уровень шума 
+       showNoise(str);
+       goto rep;
+    }
+    
+    if(str[0] == 's' || str[0] == 'S') {
+      if(str[1] == 'e') statErase();
+      else statShow(str[1]);  // Печать статистики
+      goto rep;
+    }
+    
+    if(str[0] == 'q' || str[0] == 'Q') return;     // Q - то quit
+    reg=atoi(str);
+    if(reg<0 || reg>REGS_NUM) continue; 
+
+    getStr(str);
+    if(str[0] == 'q' || str[0] == 'Q') return;     // Q - то quit
+    val=atoi(str);
+    if(val<0 || val>255) continue; 
+    if(reg == 0 && val ==0) continue;              // избегаем потери s/n
+
+    Serial.print(reg); Serial.write('=');   Serial.println(val);  // Отобразим полученное
+    
+     write_eeprom_uchar(reg,val);  // пишем регистр
+     read_eeprom();                // читаем из EEPROM    
+     write_eeprom();               // и тут-же пишем, что-бы сформировать КС 
+  }    
+}  
+
+char htxt1[] PROGMEM = "\r\nBaychi soft 2013";
+char htxt3[] PROGMEM = "RX Open Expert V2 F";
+void printHeader(void)
+{
+  printlnPGM(htxt1);
+  printlnPGM(htxt3,0); Serial.println(version[0]);
+}  
+
